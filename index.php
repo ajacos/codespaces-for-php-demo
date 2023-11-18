662c7269ad98ef1401b25b47972bcd1dac4a32a4
ajacos <151356574+ajacos@users.noreply.github.com>
2023-11-18 20:54:28 +0000

    BRUH

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
 echo "Hello World";
 
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12663220";
-$password = "V6FYSp57H4";
-$dbname = "sql12663220";
 
-// Create connection
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-$stmt = $conn->prepare('SELECT * FROM users');
-$stmt->execute();
-foreach ($stmt as $row) {
-  $id = $row['id'];
-  $name = $row["Name"];
-  $email = $row["email"];
-  $age = $row["age"];
-}
-
-echo $id."<br>".$name."<br>".$email."<br>".$age."<br>";
 ?>
+
+<p>bawal raw ang database amp</p>
